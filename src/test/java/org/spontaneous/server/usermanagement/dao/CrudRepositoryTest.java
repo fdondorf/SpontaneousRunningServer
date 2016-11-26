118c24a3854fc4cb17494472fa9b39f2d419b680
Florian Dondorf <florian.dondorf@capgemini.com>
2016-11-26 16:33:46 +0100

    Code polishing

--- src/test/java/org/spontaneous/server/usermanagement/dao/CrudRepositoryTest.java
+++ src/test/java/org/spontaneous/server/usermanagement/dao/CrudRepositoryTest.java
@@ -3,17 +3,8 @@ package org.spontaneous.server.usermanagement.dao;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.spontaneous.server.usermanagement.dao.CustomerRepository;
 import org.spontaneous.server.usermanagement.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit Test for the Crud-Repository
